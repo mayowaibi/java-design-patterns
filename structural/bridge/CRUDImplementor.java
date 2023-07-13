af8af19ca66cc9d2a04709c9129c5771f68b6986
mayowaibi <isaac.ibidun@gmail.com>
2023-07-12 22:04:17 -0400

    moved files to src

--- structural/bridge/CRUDImplementor.java
+++
@@ -1,9 +0,0 @@
-package bridge;
-
-public interface CRUDImplementor<ID, DATA> {
-
-	void postResource(DATA record, ID idInfo);
-	DATA getResource(ID idInfo);
-	void putResource(DATA record, ID idInfo);
-	void deleteResource(ID idInfo);
-}
--- structural/composite/MigratorySpecies.java
+++
@@ -1,7 +0,0 @@
-package composite;
-
-public interface MigratorySpecies {
-	
-	void migrate();
-
-}
